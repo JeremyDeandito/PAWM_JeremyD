3fde49e90001152f443d959d2734ef9d614740b0
JeremyDeandito <deanditojeremy@gmail.com>
2024-10-24 20:46:51 +0700

    Patch 0.1

--- periodicscript.js
+++ periodicscript.js
@@ -16,9 +16,69 @@ const elementsData = {
         atomicNumber: 2,
         atomicWeight: '4.0026'
     },
-    // Tambahkan lebih banyak elemen di sini
+    'Li': {
+        name: 'Lithium',
+        sifat: 'Logam Alkali',
+        symbol: 'Li',
+        img: "https://storage.googleapis.com/search-ar-edu/periodic-table/element_003_lithium/element_003_lithium_srp_th.png",
+        atomicNumber: 3,
+        atomicWeight: '6.94'
+    },
+    'Be': {
+        name: 'Beryllium',
+        sifat: 'Logam Alkali Tanah',
+        symbol: 'Be',
+        img: "https://storage.googleapis.com/search-ar-edu/periodic-table/element_004_beryllium/element_004_beryllium_srp_th.png",
+        atomicNumber: 4,
+        atomicWeight: '9.0122'
+    },
+    'B': {
+        name: 'Boron',
+        sifat: 'Metaloid',
+        symbol: 'B',
+        img: "https://storage.googleapis.com/search-ar-edu/periodic-table/element_005_boron/element_005_boron_srp_th.png",
+        atomicNumber: 5,
+        atomicWeight: '10.81'
+    },
+    'C': {
+        name: 'Carbon',
+        sifat: 'Non-Logam',
+        symbol: 'C',
+        img: "https://storage.googleapis.com/search-ar-edu/periodic-table/element_006_carbon/element_006_carbon_srp_th.png",
+        atomicNumber: 6,
+        atomicWeight: '12.011'
+    },
+    'N': {
+        name: 'Nitrogen',
+        sifat: 'Non-Logam',
+        symbol: 'N',
+        img: "https://storage.googleapis.com/search-ar-edu/periodic-table/element_007_nitrogen/element_007_nitrogen_srp_th.png",
+        atomicNumber: 7,
+        atomicWeight: '14.007'
+    },
+    'O': {
+        name: 'Oxygen',
+        sifat: 'Non-Logam',
+        symbol: 'O',
+        img: "https://storage.googleapis.com/search-ar-edu/periodic-table/element_008_oxygen/element_008_oxygen_srp_th.png",
+        atomicNumber: 8,
+        atomicWeight: '15.999'
+    },
+    'F': {
+        name: 'Fluorine',
+        sifat: 'Halogen',
+        symbol: 'F',
+        img: "https://storage.googleapis.com/search-ar-edu/periodic-table/element_009_fluorine/element_009_fluorine_srp_th.png",
+        atomicNumber: 9,
+        atomicWeight: '18.998'
+    },
+    'Ne': {
+        name: 'Neon',
+        sifat: 'Gas Mulia',
+        symbol: 'Ne',
+        img: "https://storage.googleapis.com/search-ar-edu/periodic-table/element_010_neon/
+    }
 };
-
 // Fungsi untuk menampilkan informasi elemen
 function showElementInfo(symbol) {
     const element = elementsData[symbol];
